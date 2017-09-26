a31fa77da60e2c60d345c9dced6f779a9071c762
Nikhil Singhal <niksinghal@paypal.com>
2017-09-26 18:24:00 -0400

    Adding Security Module and login related functions

---
+++ src/main/kotlin/Controllers/SessionController.kt
@@ -0,0 +1,26 @@
+package Controllers
+
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.*
+
+/**
+ * Class SessionController
+ *
+ * @author Nikhil Singhal
+ */
+@RestController
+class SessionController {
+
+    @RequestMapping(value = "/login", method = arrayOf(RequestMethod.PUT))
+    fun login(@RequestParam(required = true) userName: String, @RequestParam(required = true) userPassword: String): String {
+        //ToDo: Figure out login logic
+        return "redirect:/loginFailure"
+    }
+
+    @RequestMapping(value = "/loginFailure", method = arrayOf(RequestMethod.GET))
+    fun loginFailure(): String = "Failed to Login to Park App\n"
+
+    @PreAuthorize("hasRole('USER')")
+    @RequestMapping(value = "/logout", method = arrayOf(RequestMethod.GET))
+    fun logout(): String = "Logged Out of Park App\n"
+}

0ab390b867bd00176c0df505e8387210f1204235
Nikhil Singhal <niksinghal@paypal.com>
2017-09-28 19:56:38 -0400

    Added the details

--- src/main/kotlin/Entities/UserLogin.kt
+++ src/main/kotlin/Entities/UserLogin.kt
@@ -1,10 +1,16 @@
 package Entities
 
+import javax.persistence.Entity
+import javax.persistence.Id
+
 /**
  * class UserLogin
  *
  * @author niksinghal
  */
 
-class UserLogin {
-}
+@Entity
+data class UserLogin(
+        @Id val userId: String,
+        val password: String
+) {}

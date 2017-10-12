35b3933112fce14fd4d1c2bd809aef6c977a7bd1
Nikhil Singhal <niksinghal@paypal.com>
2017-10-11 23:00:38 -0400

    Moving Application out

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,23 @@
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@SpringBootApplication
+@ComponentScan("Controllers")
+@EntityScan("Entities")
+@EnableJpaRepositories("Repositories")
+open class Application {
+
+    private val log = LoggerFactory.getLogger(Application::class.java)
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
